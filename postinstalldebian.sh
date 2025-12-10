618b5133a393a6b8b5724c6a155c02d7de032967
Thomas CHERRIER <75333938+thomascherrier@users.noreply.github.com>
2025-12-10 12:04:10 +0100

    v 1.1

--- postinstalldebian.sh
+++ postinstalldebian.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-clear
-echo test
+apt update && apt upgrade -y
+
+apt install ssh -y
+apt install zip -y 
+apt install nmap -y 
+apt install locate -y
+upbadedb
+apt install ncdu -y
+apt install curl -y 
+apt install git -y
+apt install screen -y 
+apt install dnsutils -y 
+apt install net-tools -y 
+apt install sudo -y 
+apt install lynx -y
+
+apt install bsdgames -y
+
+apt install winbind samba -y
+
+curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+sh webmin-setup-repo.sh
+apt install webmin --install-recommends
+
+echo "Terminé"
+read -p "..." -t 3
+reboot
